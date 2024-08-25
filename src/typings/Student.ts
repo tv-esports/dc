4f39f8ec432e9a22ceaefb4089142aef6a658aba
vKxni <Konradtoesch@icloud.com>
2024-08-25 15:24:44 +0200

    ??

--- src/typings/Student.ts
+++
@@ -1,9 +0,0 @@
-export type StudentType = {
-    discord_id: string;
-    email: Array<string>;
-    customer_id: string;
-    offers: Array<string>;
-    roles: Array<string>;
-    inserted_at: Date;
-    updated_at: Date;
-}
